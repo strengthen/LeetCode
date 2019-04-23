f2ee13e526ed6813c9b98d5dc5460332fbb47c8f
strenghten <strengthen@users.noreply.github.com>
2019-04-23 12:20:03 +0800

    refresh
    refresh

--- C++/101.cpp
+++ C++/101.cpp
@@ -1,5 +1,99 @@
 __________________________________________________________________________________________________
+8ms
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true; 
+        return isSymmetricHelper(root->left,root->right);
+    }
+   bool isSymmetricHelper(TreeNode* root1, TreeNode* root2)
+    {
+       queue<TreeNode*> myqueue ; 
+       
+       myqueue.push(root1);
+       myqueue.push(root2);
+       TreeNode* temp1;
+       TreeNode* temp2;
+       while(!myqueue.empty())
+       {
+           root1 = myqueue.front();
+           myqueue.pop();
+           root2 = myqueue.front();
+           myqueue.pop();
+           
+           if(root1 == NULL && root2 == NULL)
+               continue;
+           if(root1 == NULL || root2 == NULL)
+               return false;
+           
+           if(root1->val != root2->val)
+               return false;
+           
+           myqueue.push(root1->left);
+           myqueue.push(root2->right);
+           myqueue.push(root1->right);
+           myqueue.push(root2->left);
 
+       }
+       return true;
+       
+    }
+};
 __________________________________________________________________________________________________
+14684 kb
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+static auto _______ = [](){
+	// turn off sync
+	std::ios::sync_with_stdio(false);
+	// untie in/out streams
+	std::cin.tie(nullptr);
+	return 0;
+}();
+#include <stack>
+#include <algorithm>
+class Solution {
+public:
+    bool isMirror(struct TreeNode *root1, struct TreeNode *root2) 
+    { 
+        // If both trees are emptu, then they are mirror images 
+        if (root1 == NULL && root2 == NULL) 
+            return true; 
 
+        // For two trees to be mirror images, the following three 
+        // conditions must be true 
+        // 1 - Their root node's key must be same 
+        // 2 - left subtree of left tree and right subtree 
+        //      of right tree have to be mirror images 
+        // 3 - right subtree of left tree and left subtree 
+        //      of right tree have to be mirror images 
+        if (root1 && root2 && root1->val == root2->val) 
+            return isMirror(root1->left, root2->right) && 
+                   isMirror(root1->right, root2->left); 
+
+        // if neither of above conditions is true then root1 
+        // and root2 are not mirror images 
+        return false; 
+    } 
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root, root); 
+    }
+};
 __________________________________________________________________________________________________
--- C++/102.cpp
+++ C++/102.cpp
@@ -1,5 +1,80 @@
 __________________________________________________________________________________________________
-
+8ms
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+        vector<vector<int> > levelOrder(TreeNode* pRoot) {
+            vector<pair<int,int>> vec;
+            vector<vector<int>> ans;
+            if(pRoot==nullptr)
+                return ans;
+            queue<pair<TreeNode*,int>> q;
+            q.push(make_pair(pRoot,1));
+            while(!q.empty()){
+                pair<TreeNode*,int> now=q.front();
+                q.pop();
+                vec.push_back(make_pair(now.first->val,now.second));
+                if(now.first->left)
+                    q.push(make_pair(now.first->left,now.second+1));
+                if(now.first->right)
+                    q.push(make_pair(now.first->right,now.second+1));
+            }
+            vector<int> v;
+            for(int i=0;i<vec.size();i++){
+                if(i!=0&&vec[i].second!=vec[i-1].second){
+                    ans.push_back(v);
+                    v.clear();
+                }
+                v.push_back(vec[i].first);
+            }
+            ans.push_back(v);
+            return ans;
+        }
+};
 __________________________________________________________________________________________________
-
+13532 kb
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if (!root) return {};
+        vector<vector<int>> result{1, vector<int>{}};
+        queue<TreeNode*> nodes{};
+        nodes.push(root);
+        nodes.push(nullptr);
+        int resultIndex = 0;
+        while(!nodes.empty()) {
+            auto next = nodes.front();
+            nodes.pop();
+            if (!next) {
+                if (nodes.empty()) break;
+                nodes.push(nullptr);
+                ++resultIndex;
+                result.push_back(vector<int>{});
+            } else {
+                result[resultIndex].push_back(next->val);
+                if (next->left) nodes.push(next->left);
+                if (next->right) nodes.push(next->right);
+            }
+        }
+        return result;
+    }
+};
 __________________________________________________________________________________________________
