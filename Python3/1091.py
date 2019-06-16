b581648c9a56c6618c9d27df7070397a25d0ece9
strenghten <strengthen@users.noreply.github.com>
2019-06-16 14:56:40 +0800

    refresh
    refresh

--- Python3/1091.py
+++ Python3/1091.py
@@ -1,5 +1,23 @@
 __________________________________________________________________________________________________
-
+class Solution:
+    def shortestPathBinaryMatrix(self, A: List[List[int]]) -> int:
+        if A[0][0]: return -1
+        dq = collections.deque([(0, 0)])
+        seen = {(0, 0)}
+        step = 1
+        n = len(A)
+        while dq:
+            sz = len(dq)
+            for _ in range(sz):
+                x, y = dq.popleft()
+                for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (-1, -1), (1, -1)):
+                    xx, yy = x + dx, y + dy
+                    if 0 <= xx < n and 0 <= yy < n and A[xx][yy] == 0 and (xx, yy) not in seen:
+                        seen.add((xx, yy))
+                        if (xx, yy) == (n - 1, n - 1): return step + 1
+                        dq.append((xx, yy))
+            step += 1
+        return -1
 __________________________________________________________________________________________________
 
 __________________________________________________________________________________________________
