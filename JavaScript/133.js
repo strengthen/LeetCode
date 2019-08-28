7e9ec86be99b8b21bfa4aafd2f6d7a22a29d0156
ws1255@nyu.edu <ws1255@nyu.edu>
2019-08-28 16:22:08 -0400

    Update 133.js BFS version
    add BFS version of this question

--- JavaScript/133.js
+++ JavaScript/133.js
@@ -33,5 +33,39 @@ var cloneGraph = function(node) {
 };
 
 __________________________________________________________________________________________________
+sample 2 BFS Runtime: 88 ms Memory Usage: 44.4 MB
+/**
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    if(node == null) return null;
 
+    let map = {};
+    let queue = [];
+
+    let clone = new Node(node.val, []);
+    map[node.val] = clone;
+    queue.push(node);
+
+    while(queue.length > 0) {
+        let cur = queue.shift();
+        for(let neighbor of cur.neighbors) {
+            if(map[neighbor.val] == undefined) {
+                map[neighbor.val] = new Node(neighbor.val, []);
+                queue.push(neighbor);
+            }
+            map[cur.val].neighbors.push(map[neighbor.val]);
+        }
+    }
+
+    return clone;
+}
 __________________________________________________________________________________________________
