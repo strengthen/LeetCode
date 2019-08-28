672778eb415a800372e5714006420a017abc28a0
ws1255@nyu.edu <ws1255@nyu.edu>
2019-08-28 00:46:39 -0400

    Update 133.js

--- JavaScript/133.js
+++ JavaScript/133.js
@@ -1,4 +1,36 @@
 __________________________________________________________________________________________________
+sample 1 DFS Runtime: 92 ms Memory Usage: 44.8 MB
+/**
+ * // Definition for a Node.
+ * function Node(val,neighbors) {
+ *    this.val = val;
+ *    this.neighbors = neighbors;
+ * };
+ */
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+var cloneGraph = function(node) {
+    if(!node) return node;
+
+    var map = {};
+
+    return dfs(node);
+
+    function dfs(node) {
+        if(!node) return node;
+        if(map[node.val] != undefined) return map[node.val];
+        let clone = new Node(node.val, []);
+        map[node.val] = clone;
+
+        for(let neighbor of node.neighbors) {
+            clone.neighbors.push(dfs(neighbor));
+        }
+
+        return map[node.val];
+    }
+};
 
 __________________________________________________________________________________________________
 
