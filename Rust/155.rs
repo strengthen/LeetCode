1640586c1b561d54a05be562eac7bfe1d21777e6
Blazeboard <blazeboard@outlook.com>
2021-11-11 22:22:18 +0800

    add some solution in rust

--- Rust/155.rs
+++ Rust/155.rs
@@ -1,5 +1,58 @@
 __________________________________________________________________________________________________
+//runtime: 4ms, memory: 5.9MB
 
+struct MinStack {
+    stack: Vec<i32>,
+    min_stack: Vec<i32>,
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+
+    fn new() -> Self {
+        MinStack {
+            stack: Vec::new(),
+            min_stack: Vec::new(),
+        }
+    }
+    
+    fn push(&mut self, val: i32) {
+        self.stack.push(val);
+        if self.min_stack.is_empty() || val <= *self.min_stack.last().unwrap() {
+            self.min_stack.push(val);
+        }
+    }
+    
+    fn pop(&mut self) {
+        if self.min_stack.is_empty() {
+            return;
+        }
+        if self.stack.pop().unwrap() == *self.min_stack.last().unwrap() {
+            self.min_stack.pop();
+        }
+    }
+    
+    fn top(&self) -> i32 {
+        return *self.stack.last().unwrap();
+    }
+    
+    fn get_min(&self) -> i32 {
+        return *self.min_stack.last().unwrap();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * let obj = MinStack::new();
+ * obj.push(val);
+ * obj.pop();
+ * let ret_3: i32 = obj.top();
+ * let ret_4: i32 = obj.get_min();
+ */
 __________________________________________________________________________________________________
 
 __________________________________________________________________________________________________
--- Rust/51.rs
+++ Rust/51.rs
@@ -1,5 +1,49 @@
 __________________________________________________________________________________________________
+//runtime: 4ms, memory: 2.2MB
 
+impl Solution {
+    pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
+        let mut board = vec![vec!['.'; n as usize]; n as usize];
+        let mut solution = vec![];
+        backtrack(&mut board, &mut solution, n, 0);
+        solution
+    }
+}
+
+fn backtrack(board: &mut Vec<Vec<char>>, solution: &mut Vec<Vec<String>>, n:i32, row: i32) {
+    for column in 0..n {
+        if !collision(&board, n, row, column) {
+            board[row as usize][column as usize] = 'Q';
+            if row == n - 1 {
+                solution.push(board.iter().map(|vec| vec.iter().collect()).collect());
+            } else {
+                backtrack(board, solution, n, row + 1);
+            }
+            board[row as usize][column as usize] = '.';
+        }
+    }
+}
+
+fn collision(board: &Vec<Vec<char>>, n: i32, row: i32, column: i32) ->bool {
+    let mut up_row = row - 1;
+    let mut left_column = column - 1;
+    let mut right_column = column + 1;
+    while up_row >= 0 {
+        if board[up_row as usize][column as usize] == 'Q' {
+            return true;
+        }
+        if left_column >= 0 && board[up_row as usize][left_column as usize] == 'Q' {
+            return true;
+        }
+        if right_column < n && board[up_row as usize][right_column as usize] == 'Q' {
+            return true;
+        }
+        up_row -= 1;
+        left_column -= 1;
+        right_column += 1;
+    }
+    false
+}
 __________________________________________________________________________________________________
 
 __________________________________________________________________________________________________
